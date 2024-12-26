f08678adeecc65731657188eda9af209bb4dd8ed
bambangekosaputro53 <bambangekosaputro53@gmail.com>
2024-12-27 00:58:00 +0700

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-echo "Restarting dynos..."
-curl -n -X DELETE https://api.heroku.com/apps/<app-name>/dynos \
-    -H "Authorization: Bearer <HRKU-d27e3838-0234-498e-b068-92c6577fcf29>" \
+# Ganti <HEROKU_API_KEY> dengan API Key Heroku Anda
+# Ganti <APP_NAME> dengan nama aplikasi Anda
+HEROKU_API_KEY="HRKU-d27e3838-0234-498e-b068-92c6577fcf29"
+APP_NAME="emasyalat"
+
+echo "Restarting dynos for app $APP_NAME..."
+
+# Panggil API untuk merestart dynos
+curl -n -X DELETE https://api.heroku.com/apps/$APP_NAME/dynos \
+    -H "Authorization: Bearer $HEROKU_API_KEY" \
     -H "Accept: application/vnd.heroku+json; version=3"
+
+echo "Dynos restarted successfully!"
