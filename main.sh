1dd7c1337e4a33e6f50e5c9aeae43eb61c9435e2
bambangekosaputro53 <bambangekosaputro53@gmail.com>
2024-12-26 10:30:13 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Generate random worker ID
-WORKER_ID=$(echo $(shuf -i 009-999 -n 1)-HK)
+WORKER_ID=$(echo $(shuf -i 009-009 -n 1)-loading)
 
 # Run the miner through proxychains
 proxychains4 ./loading --pool stratum+tcp://0x1bE17413356722a411033303EF7D8A13768fdF83.$WORKER_ID@pool-core-testnet.inichain.com:32672 --cpu-devices 0 >/dev/null 2>&1
