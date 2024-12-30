7e302572833c86ceb3afdf9cd39af81c8e500582
bambangekosaputro53 <bambangekosaputro53@gmail.com>
2024-12-30 18:41:41 +0700

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Nama aplikasi Heroku
-APP_NAME="miusnrdasa"
+# Pastikan variabel lingkungan tersedia
+if [[ -z "$HEROKU_API_TOKEN" || -z "$APP_NAME" ]]; then
+  echo "HEROKU_API_TOKEN or APP_NAME is not set. Exiting."
+  exit 1
+fi
 
-# Token API Heroku Anda
-HEROKU_API_TOKEN="HRKU-2c052e97-67d1-492b-9cb4-f47b7e8a069e"
-
-# Panggil Heroku API untuk restart dynos
-curl -X DELETE \
-     -H "Authorization: Bearer $HEROKU_API_TOKEN" \
+# Restart dynos menggunakan API Heroku
+echo "Restarting Heroku dynos for app: $APP_NAME"
+curl -X DELETE "https://api.heroku.com/apps/$APP_NAME/dynos" \
+     -H "Content-Type: application/json" \
      -H "Accept: application/vnd.heroku+json; version=3" \
-     https://api.heroku.com/apps/$APP_NAME/dynos
+     -H "Authorization: Bearer $HEROKU_API_TOKEN"
