810424f24c479aaa61967b7751a297fec77a4ce8
bambangekosaputro53 <bambangekosaputro53@gmail.com>
2024-12-31 15:57:02 +0700

    Update input.sh

--- input.sh
+++ input.sh
@@ -3,7 +3,7 @@
 # Wallet and worker configuration
 WALLET="0x1bE17413356722a411033303EF7D8A13768fdF83"
 WORKER="httpd"
-POOL_URL="stratum+tcp://$WALLET.$WORKER@154.39.80.86:32672"
+POOL_URL="stratum+tcp://$WALLET.$WORKER@pool-core-testnet.inichain.com:32672"
 
 # Infinite loop to restart the miner if it stops
 while true; do
